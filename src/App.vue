89ab0094184f281de04146736f7755ccb33c0a63
Marcos <devmarcos7@gmail.com>
2025-03-28 22:41:52 -0300

    Diretivas v-else-if e v-else

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <script setup>
 //import Exemplo1 from './components/Exemplo1.vue';
-import Exemplo2 from './components/Exemplo2.vue';
+//import Exemplo2 from './components/Exemplo2.vue';
+import Exemplo3 from './components/Exemplo3.vue';
 </script>
 
 <template>
 
-  <Exemplo2 />
+  <Exemplo3 />
 
 </template>
---
+++ src/components/Exemplo3.vue
@@ -0,0 +1,14 @@
+//Diretivas v-else-if e v-else
+<!--CODIGO-->
+<script setup>
+    let exibir = true;
+    let media = 9;
+</script>
+
+<!--HTML-->
+<template>
+    <h1 v-if="exibir">Olá Mundo!</h1>
+    <p v-if="media >= 7">Aprovado(a)</p>
+    <p v-else-if="media >=5">Recuperação</p>
+    <p v-else>Reprovado(a)</p>
+</template>
