479fed5691373f44becbf2c247e9d45610f58101
Marcos <devmarcos7@gmail.com>
2025-03-29 09:41:01 -0300

    Diretiva :class

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,13 @@
 //import Exemplo3 from './components/Exemplo3.vue';
 //import Exemplo4 from './components/Exemplo4.vue';
 //import Exemplo5 from './components/Exemplo5.vue';
-import Exemplo6 from './components/Exemplo6.vue';
+//import Exemplo6 from './components/Exemplo6.vue';
+import Exemplo7 from './components/Exemplo7.vue';
+
 </script>
 
 <template>
 
-  <Exemplo6 />
+  <Exemplo7 />
 
 </template>
---
+++ src/components/Exemplo7.vue
@@ -0,0 +1,24 @@
+<!--Diretiva :class-->
+<script setup>
+    let validacao = true;
+    let validacao2 = false;
+</script>
+
+<style>
+    .ok {
+        color:green;
+    }
+
+    .falha {
+        color: red;
+    }
+</style>
+
+<template>
+    <h1 :class="validacao ? 'ok' : 'falha'">
+        Diretiva :class
+    </h1>
+    <h1 :class="validacao2 ? 'ok' : 'falha'">
+        Diretiva :class 2
+    </h1>
+</template>
