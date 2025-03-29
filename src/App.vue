c994f001a3cd42e44f53da2f2fd02d7535275b05
Marcos <devmarcos7@gmail.com>
2025-03-29 09:27:20 -0300

    Diretiva v-model

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,12 @@
 //import Exemplo2 from './components/Exemplo2.vue';
 //import Exemplo3 from './components/Exemplo3.vue';
 //import Exemplo4 from './components/Exemplo4.vue';
-import Exemplo5 from './components/Exemplo5.vue';
+//import Exemplo5 from './components/Exemplo5.vue';
+import Exemplo6 from './components/Exemplo6.vue';
 </script>
 
 <template>
 
-  <Exemplo5 />
+  <Exemplo6 />
 
 </template>
---
+++ src/components/Exemplo6.vue
@@ -0,0 +1,11 @@
+Diretiva v-model
+<script setup>
+import { ref } from 'vue';
+
+    let texto = ref('');
+</script>
+
+<template>
+    <input type="text" v-model="texto">
+    <h1>{{ texto }}</h1>
+</template>
