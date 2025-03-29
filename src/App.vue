00258ca823db638fb9c95bc30959567711ba70ba
Marcos <devmarcos7@gmail.com>
2025-03-29 07:35:23 -0300

    Diretiva v-for

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <script setup>
 //import Exemplo1 from './components/Exemplo1.vue';
 //import Exemplo2 from './components/Exemplo2.vue';
-import Exemplo3 from './components/Exemplo3.vue';
+//import Exemplo3 from './components/Exemplo3.vue';
+import Exemplo4 from './components/Exemplo4.vue';
 </script>
 
 <template>
 
-  <Exemplo3 />
+  <Exemplo4 />
 
 </template>
---
+++ src/components/Exemplo4.vue
@@ -0,0 +1,10 @@
+//Diretiva v-for
+<script setup>
+    let nomes = ["Alice", "Bruna", "Caio", "Daniel", "Ellen"];
+</script>
+
+<template>
+    <ul>
+        <li v-for="nome in nomes">{{ nome }}</li>
+    </ul>
+</template>
