a542d976040941b337fab44635227e78a91359f9
Marcos <devmarcos7@gmail.com>
2025-03-29 08:49:43 -0300

    Diretiva v-html

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,12 @@
 //import Exemplo1 from './components/Exemplo1.vue';
 //import Exemplo2 from './components/Exemplo2.vue';
 //import Exemplo3 from './components/Exemplo3.vue';
-import Exemplo4 from './components/Exemplo4.vue';
+//import Exemplo4 from './components/Exemplo4.vue';
+import Exemplo5 from './components/Exemplo5.vue';
 </script>
 
 <template>
 
-  <Exemplo4 />
+  <Exemplo5 />
 
 </template>
---
+++ src/components/Exemplo5.vue
@@ -0,0 +1,8 @@
+//Diretiva v-html
+<script setup>
+    let texto = '<p>Aprendendo a diretiva v-html</p>';
+</script>
+
+<template>
+    <div v-html="texto"></div>
+</template>
